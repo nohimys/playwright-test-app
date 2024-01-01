d86cf1ff9108b6416f7fe3ad77e2abbd48600a21
nohimgodage <nohim.godage@gmail.com>
2024-01-01 16:27:07 +0530

    User facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -7,7 +7,7 @@ test.beforeEach(async ({page}) => {
     await page.getByText('Form Layouts').click();
 });
 
-test('Locator syntax rules', async ({page}) => {
+test('locator syntax rules', async ({page}) => {
     //Find all exists by Tag Name
     await page.locator('input').first().click();
 
@@ -37,4 +37,26 @@ test('Locator syntax rules', async ({page}) => {
     //Locate by exact text match
     page.locator(':text-is("Using the Grid")');
 
+});
+
+test.only('user facing locators', async ({page}) => {
+
+    //It's good to always start with the role
+    //It's the usual user thought process
+    await page.getByRole('textbox', {name: 'Email'}).first().click();
+    await page.getByRole('button', {name: 'Sign In'}).first().click();
+
+    //I think this is referring the HTML for value to do the mapping
+    await page.getByLabel('Email').first().click();
+
+    await page.getByPlaceholder('Jane Doe').first().click();
+
+    //We can use any static text we see on Screen here
+    await page.getByText('Using the Grid').first().click();
+
+    //Title is also an HTML attribute
+    await page.getByTitle('IoT Dashboard').first().click();
+
+    //This id is a PlayWright reserved id defined in the source code itself
+    await page.getByTestId('sing-in').first().click();
 });
