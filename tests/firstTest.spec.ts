003658e24c75ea3100028437dccb0d4cb5e4fe05
nohimgodage <nohim.godage@gmail.com>
2024-01-01 15:59:22 +0530

    Available locators with PlayWright

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,12 +4,37 @@ import {test} from '@playwright/test'
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200');
     await page.getByText('Forms').click();
-});
-
-test('test 1', async ({page}) => {
     await page.getByText('Form Layouts').click();
 });
 
-test('navigate to datepicker page', async ({page}) => {
-    await page.getByText('Datepicker').click();
+test('Locator syntax rules', async ({page}) => {
+    //Find all exists by Tag Name
+    await page.locator('input').first().click();
+
+    //Find by Id
+    page.locator('#inputEmail1');
+
+    //Find by Class Value
+    page.locator('.shape-rectangle');
+
+    //Find by attribute
+    page.locator('[placeholder="Email"]');
+
+    //Find by Entire Class Value
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+
+    //Combine different selector
+    //Remember NOT to put spaces between selectors
+    page.locator('input.shape-rectangle[placeholder="Email"][type="email"][nbinput]');
+
+    //Find by XPath
+    //NOT Recommended
+    page.locator('//*[@id="inputEmail1"]');
+
+    //Locate by partial text match
+    page.locator(':text("Using")');
+
+    //Locate by exact text match
+    page.locator(':text-is("Using the Grid")');
+
 });
