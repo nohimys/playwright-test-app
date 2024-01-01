1bb08c0fc1b4fa8b9c1de03037ad8cec98e922ba
nohimgodage <nohim.godage@gmail.com>
2024-01-01 15:09:52 +0530

    Usage of beforeEach

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,15 @@
 import {test} from '@playwright/test'
 
-test('test 1', async ({page}) => {
+
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200');
     await page.getByText('Forms').click();
+});
+
+test('test 1', async ({page}) => {
     await page.getByText('Form Layouts').click();
+});
+
+test('navigate to datepicker page', async ({page}) => {
+    await page.getByText('Datepicker').click();
 });
