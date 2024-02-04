fddb47f8365043635748d28611345697b0d5b08d
nohimgodage <nohim.godage@gmail.com>
2024-02-04 15:00:27 +0530

    Moving Locators to class level in Page Object Model

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,30 +1,42 @@
-import {Page} from "@playwright/test";
+import {Locator, Page} from "@playwright/test";
 
 export class NavigationPage {
     private readonly page: Page;
+
+    private readonly formLayoutsMenuItem: Locator;
+    private readonly datepickerMenuItem: Locator;
+    private readonly smartTableMenuItem: Locator;
+    private readonly toastrMenuItem: Locator;
+    private readonly tooltipMenuItem: Locator;
     constructor(page: Page) {
         this.page = page;
+
+        this.formLayoutsMenuItem = this.page.getByText('Form Layouts');
+        this.datepickerMenuItem = this.page.getByText('Datepicker');
+        this.smartTableMenuItem = this.page.getByText('Smart Table');
+        this.toastrMenuItem = this.page.getByText('Toastr');
+        this.tooltipMenuItem = this.page.getByText('Tooltip');
     }
     public formLayoutsPage = async () => {
         await this.selectGroupMenuItem('Forms');
-        await this.page.getByText('Form Layouts').click();
+        await this.formLayoutsMenuItem.click();
     }
 
     public datepickerPage = async () => {
         await this.selectGroupMenuItem('Forms');
-        await this.page.getByText('Datepicker').click();
+        await this.datepickerMenuItem.click();
     }
     public smartTablePage = async () => {
         await this.selectGroupMenuItem('Tables & Data');
-        await this.page.getByText('Smart Table').click();
+        await this.smartTableMenuItem.click();
     }
     public toastrPage = async () => {
         await this.selectGroupMenuItem('Modal & Overlays');
-        await this.page.getByText('Toastr').click();
+        await this.toastrMenuItem.click();
     }
     public tooltipPage = async () => {
         await this.selectGroupMenuItem('Modal & Overlays');
-        await this.page.getByText('Tooltip').click();
+        await this.tooltipMenuItem.click();
     }
 
     private selectGroupMenuItem = async (groupItemTitle: string) => {
