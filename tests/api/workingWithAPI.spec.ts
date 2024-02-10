487a3ac7b72e1bf7a750039ce000ed779a77f0ef
nohimgodage <nohim.godage@gmail.com>
2024-02-10 15:21:50 +0530

    Modifying the response from API

--- tests/api/workingWithAPI.spec.ts
+++ tests/api/workingWithAPI.spec.ts
@@ -1,8 +1,8 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 // @ts-ignore
 import tags from "./../../test-data/tags.json"
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
     //Intercept Routes from Browser to Server
     await page.route(
         '*/**/api/tags',
@@ -13,9 +13,24 @@ test.beforeEach(async ({page}) => {
             });
         });
 
+    await page.route(
+        '*/**/api/articles*',
+        async (route, req) => {
+            const response = await route.fetch();
+            const responseBodyJson = await response.json();
+            responseBodyJson.articles[0].title = 'Edited Tittle';
+            responseBodyJson.articles[0].description = 'Edited Description';
+
+            await route.fulfill({
+                body: JSON.stringify(responseBodyJson)
+            });
+        });
+
     await page.goto('https://conduit.bondaracademy.com');
 });
 
-test('title verification', async ({page}) => {
+test('title verification', async ({ page }) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+    await expect(page.locator('app-article-list h1').first()).toContainText('Edited Tittle');
+    await expect(page.locator('app-article-list p').first()).toContainText('Edited Description');
 });
