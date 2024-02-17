94f90cbe5878fb174b1d17ef15398866650da959
nohimgodage <nohim.godage@gmail.com>
2024-02-17 21:02:18 +0530

    Using .env & process.env to store Env Variable

--- tests/autoWait.spec.ts
+++ tests/autoWait.spec.ts
@@ -1,7 +1,7 @@
 import {expect, test} from '@playwright/test'
 
 test.beforeEach(async ({page}, testInfo) => {
-    await page.goto('http://uitestingplayground.com/ajax');
+    await page.goto(`${process.env.TESTING_PLAYGROUND_URL}/ajax`);
     await page.getByText('Button Triggering AJAX Request').click();
 
     testInfo.setTimeout(testInfo.timeout + 2000);
