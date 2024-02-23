6c8457aafa7cca9cc7ce81e3404a3e15f81485d7
nohimgodage <nohim.godage@gmail.com>
2024-02-23 22:35:39 +0530

    Using inbuild reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,11 @@ export default defineConfig<TestOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html','list'],
+    ['json', {outputFile: 'test-results/json-file.json'}],
+    ['junit', {outputFile: 'test-results/junit-file.xml'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
